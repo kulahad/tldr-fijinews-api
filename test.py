46db151bcb964e609b5e857cf1d9ffcb30b146d3
Mohammed Muqtadir Ahad <kulahad007@outlook.com>
2020-05-28 01:48:16 +1200

    Add time attribute to newsItem object  - to keep track of time

--- test.py
+++ test.py
@@ -1,17 +1,40 @@
 from bs4 import BeautifulSoup as bs
 import requests as req
 import sqlite3
-
+import datetime as dt
 
 class newsItem(object):
     Id = 0
     header = ''
     link = ''
+    time = ''
 
-    def __init__(self, newsId, header, link):
+    def __init__(self, newsId, header, link, time):
         self.Id = newsId
         self.header = header
         self.link = link
+        self.time = time
+
+
+def store_data_to_db(newsList):
+    conn = sqlite3.connect('News.db')
+    print("Opened database successfully")
+    conn.execute('''CREATE TABLE IF NOT EXISTS NEWS
+           (ID INT PRIMARY KEY     NOT NULL,
+           HEADER           TEXT     NULL,
+           LINK            BLOB      NULL,
+           TIME            TEXT      NULL);''')
+    print("Table created successfully")
+
+    for item in newsList:
+        query = f'INSERT OR IGNORE INTO NEWS (ID,HEADER,LINK,TIME) \
+            VALUES \
+          ({item.Id}, "{item.header}", "{item.link}","{item.time}");'
+        conn.execute(query)
+
+    conn.commit()
+    print("Records created successfully")
+    conn.close()
 
 
 fijivillage = req.get("https://fijivillage.com")
@@ -24,28 +47,25 @@ if isAvailable == 200:
     alldiv = parsed_html.find_all('div')
 
     for div in alldiv:
-        isFound = False
         if div.h6:
-            story = newsItem(id(div.h6.string), div.h6.string, div.a['href'])
+            if div.find('span'):
+                newsAge = div.find('span').text
+                timeNow = dt.datetime.now()
+                if newsAge[2] == "h":
+                    time = timeNow - dt.timedelta(hours=int(newsAge[0]))
+                elif newsAge[2] == "d":
+                    time = timeNow - dt.timedelta(days=int(newsAge[0]))
+            story = newsItem(id(div.h6.string), div.h6.string, div.a['href'], time)
             newsList.append(story)
         elif div.h3:
-            story = newsItem(id(div.h3.string), div.h3.string, div.a['href'])
+            if div.find('span'):
+                newsAge = div.find('span').text
+                timeNow = dt.datetime.now()
+                if newsAge[2] == "h":
+                    time = timeNow - dt.timedelta(hours=int(newsAge[0]))
+                elif newsAge[2] == "d":
+                    time = timeNow - dt.timedelta(days=int(newsAge[0]))
+            story = newsItem(id(div.h3.string), div.h3.string, div.a['href'], time)
             newsList.append(story)
 
-conn = sqlite3.connect('test.db')
-print("Opened database successfully")
-conn.execute('''CREATE TABLE IF NOT EXISTS NEWS
-         (ID INT PRIMARY KEY     NOT NULL,
-         HEADER           TEXT     NULL,
-         LINK            BLOB      NULL);''')
-print("Table created successfully")
-
-for item in newsList:
-    query = f'INSERT OR IGNORE INTO NEWS (ID,HEADER,LINK) \
-          VALUES \
-        ({item.Id}, "{item.header}", "{item.link}");'
-    conn.execute(query)
-
-conn.commit()
-print("Records created successfully")
-conn.close()
+store_data_to_db(newsList)
