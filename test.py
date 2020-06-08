fe2f29f64a96296fe845946464bf3a349b901e83
Mohammed Muqtadir Ahad <kulahad007@outlook.com>
2020-06-08 15:27:48 +1200

    Added functions that handle data scraping

--- test.py
+++ test.py
@@ -3,14 +3,13 @@ import requests as req
 import sqlite3
 import datetime as dt
 
+
 class newsItem(object):
-    Id = 0
     header = ''
     link = ''
     time = ''
 
-    def __init__(self, newsId, header, link, time):
-        self.Id = newsId
+    def __init__(self, header, link, time):
         self.header = header
         self.link = link
         self.time = time
@@ -20,16 +19,15 @@ def store_data_to_db(newsList):
     conn = sqlite3.connect('News.db')
     print("Opened database successfully")
     conn.execute('''CREATE TABLE IF NOT EXISTS NEWS
-           (ID INT PRIMARY KEY     NOT NULL,
-           HEADER           TEXT     NULL,
+           (HEADER     TEXT PRIMARY KEY     NULL,
            LINK            BLOB      NULL,
            TIME            TEXT      NULL);''')
     print("Table created successfully")
 
     for item in newsList:
-        query = f'INSERT OR IGNORE INTO NEWS (ID,HEADER,LINK,TIME) \
+        query = f'INSERT OR REPLACE INTO NEWS (HEADER,LINK,TIME) \
             VALUES \
-          ({item.Id}, "{item.header}", "{item.link}","{item.time}");'
+          ("{item.header}", "{item.link}","{item.time}");'
         conn.execute(query)
 
     conn.commit()
@@ -37,35 +35,36 @@ def store_data_to_db(newsList):
     conn.close()
 
 
-fijivillage = req.get("https://fijivillage.com")
-isAvailable = fijivillage.status_code
-html = fijivillage.text
-newsList = []
+def getNewsItems(url):
+    siteResponse = req.get(url)
+    isAvailable = siteResponse.status_code
+    html = siteResponse.text
+    newsList = []
 
-if isAvailable == 200:
-    parsed_html = bs(html, "html.parser")
-    alldiv = parsed_html.find_all('div')
+    if isAvailable == 200:
+        parsed_html = bs(html, "html.parser")
+        alldiv = parsed_html.find_all('div')
 
-    for div in alldiv:
-        if div.h6:
-            if div.find('span'):
-                newsAge = div.find('span').text
-                timeNow = dt.datetime.now()
-                if newsAge[2] == "h":
-                    time = timeNow - dt.timedelta(hours=int(newsAge[0]))
-                elif newsAge[2] == "d":
-                    time = timeNow - dt.timedelta(days=int(newsAge[0]))
-            story = newsItem(id(div.h6.string), div.h6.string, div.a['href'], time)
-            newsList.append(story)
-        elif div.h3:
-            if div.find('span'):
-                newsAge = div.find('span').text
-                timeNow = dt.datetime.now()
-                if newsAge[2] == "h":
-                    time = timeNow - dt.timedelta(hours=int(newsAge[0]))
-                elif newsAge[2] == "d":
-                    time = timeNow - dt.timedelta(days=int(newsAge[0]))
-            story = newsItem(id(div.h3.string), div.h3.string, div.a['href'], time)
-            newsList.append(story)
+        for div in alldiv:
+            if div.h6:
+                if div.find('span'):
+                    newsAge = div.find('span').text
+                    timeNow = dt.datetime.now()
+                    if newsAge[2] == "h":
+                        time = timeNow - dt.timedelta(hours=int(newsAge[0]))
+                    elif newsAge[2] == "d":
+                        time = timeNow - dt.timedelta(days=int(newsAge[0]))
+                newsList.append(newsItem(div.h6.string, div.a['href'], time))
+            elif div.h3:
+                if div.find('span'):
+                    newsAge = div.find('span').text
+                    timeNow = dt.datetime.now()
+                    if newsAge[2] == "h":
+                        time = timeNow - dt.timedelta(hours=int(newsAge[0]))
+                    elif newsAge[2] == "d":
+                        time = timeNow - dt.timedelta(days=int(newsAge[0]))
+                newsList.append(newsItem(div.h3.string, div.a['href'], time))
+    store_data_to_db(newsList)
 
-store_data_to_db(newsList)
+
+getNewsItems("https://fijivillage.com/")
